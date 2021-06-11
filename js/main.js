cd284fd11fcd48662bc85a4f1cd02c7b7c533da9
Vovo <edvemonist773@gmail.com>
2021-06-11 16:47:24 +0300

    Добавил две функции

---
+++ js/main.js
@@ -0,0 +1,38 @@
+function getRandomWhole(min, max) {
+  if (min < 0) {
+    min = 0;
+  }
+
+  if (max <= min) {
+    max = min + 1;
+  }
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+}
+
+function getRandomFractional(min, max, quantity) {
+  if (min < 0) {
+    min = 0;
+  }
+
+  if (max <= min) {
+    max = min + 1;
+  }
+
+  if (quantity < 0) {
+    quantity = 1;
+  }
+
+  if (quantity > 20) {
+    quantity = 20;
+  }
+
+  return (Math.random() * (max - min) + min).toFixed(quantity);
+}
+
+
+getRandomWhole();
+getRandomFractional();
+
